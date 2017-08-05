e3a93c171f5e52a49024c7c6698b4bf20dcc9c33
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-05 23:43:14 +0530

    Update middleware-rasa.js

--- middleware-rasa.js
+++ middleware-rasa.js
@@ -35,7 +35,7 @@ module.exports = config => {
 };
 */
       
-      let postData = { 'q': 'hi'};
+      let postData = { 'q': message.text};
   
       
       debug('Sending message to Rasa', message.text)
