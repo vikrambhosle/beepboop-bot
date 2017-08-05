ec486856987accca7ac337b72aca9b3da990988f
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-05 22:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var token = process.env.SLACK_TOKEN
 var controller = Botkit.slackbot({
   // reconnect to Slack RTM when connection goes bad
   retry: Infinity,
-  debug: false
+  debug: true
 })
 
 controller.middleware.receive.use(rasa.receive);
