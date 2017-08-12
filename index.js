124707d755c94e61bd8d4431ab5e4de6c525271b
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-12 19:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,13 +49,14 @@ controller.hears(['create_wp'],'direct_message,direct_mention,mention', rasa.hea
       {
         pattern: ['.'],
         callback: function(response,onvo) {
-          console.log('PIntent:', response.intent);
-          /*if( response.intent.name=='dont_know') {
+          console.log('PIntent:', response.intent, response.intent.name);
+          if(response.intent.name=='dont_know') {
           onvo.say('I need a description to proceed');
           onvo.repeat();
+            onvo.next();
           }
           else{
-          onvo.gotoThread('wpjira')}*/
+          onvo.gotoThread('wpjira')}
               } }
         /* {
         default: true,
