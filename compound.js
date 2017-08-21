a39afae3f4f3d48ed125633b1dba9b6fd154a847
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-21 13:40:56 +0530

    Update compound.js

--- compound.js
+++ compound.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
 
-var meta= '[{ent:"amount", search:"entities", type: "amount-of-money"},{ent:"headcount", search:"entities", type: "number"},{ent:"strtdate", search:"additional_info.grain", type: "day"}]'
+var meta= '[{ent:"amount", search:"entities", type: "amount-of-money"},{ent:"headcount", search:"entities", type: "number"},{ent:"startdate", search:"additional_info.grain", type: "day"},{ent:"enddate", search:"additional_info.grain", type: "day"},{ent:"jira", search:"entities", type: "number"}]'
 var str = '{"model": "default", "time": "2017-08-19T13:09:40.445117", "user_input": {"entities": [{"end": 51, "entity": "headcount", "extractor": "ner_crf", "start": 45, "value": "12 dec"}, {"end": 72, "entity": "amount", "extractor": "ner_crf", "start": 61, "value": "4000 pounds"}, {"additional_info": {"value": 12.0}, "end": 47, "entity": "number", "extractor": "ner_duckling", "start": 45, "text": "12", "value": 12.0}, {"additional_info": {"value": 2018.0}, "end": 56, "entity": "number", "extractor": "ner_duckling", "start": 52, "text": "2018", "value": 2018.0}, {"additional_info": {"value": 4000.0}, "end": 65, "entity": "number", "extractor": "ner_duckling", "start": 61, "text": "4000", "value": 4000.0}, {"additional_info": {"unit": "\u00a3", "value": 4000.0}, "end": 72, "entity": "amount-of-money", "extractor": "ner_duckling", "start": 61, "text": "4000 pounds", "value": 4000.0}, {"additional_info": {"unit": null, "value": 12.0}, "end": 47, "entity": "distance", "extractor": "ner_duckling", "start": 45, "text": "12", "value": 12.0}, {"additional_info": {"unit": null, "value": 2018.0}, "end": 56, "entity": "distance", "extractor": "ner_duckling", "start": 52, "text": "2018", "value": 2018.0}, {"additional_info": {"unit": null, "value": 4000.0}, "end": 65, "entity": "distance", "extractor": "ner_duckling", "start": 61, "text": "4000", "value": 4000.0}, {"additional_info": {"latent": true, "unit": null, "value": 12.0}, "end": 47, "entity": "volume", "extractor": "ner_duckling", "start": 45, "text": "12", "value": 12.0}, {"additional_info": {"latent": true, "unit": null, "value": 2018.0}, "end": 56, "entity": "volume", "extractor": "ner_duckling", "start": 52, "text": "2018", "value": 2018.0}, {"additional_info": {"latent": true, "unit": null, "value": 4000.0}, "end": 65, "entity": "volume", "extractor": "ner_duckling", "start": 61, "text": "4000", "value": 4000.0}, {"additional_info": {"unit": null, "value": 12.0}, "end": 47, "entity": "temperature", "extractor": "ner_duckling", "start": 45, "text": "12", "value": 12.0}, {"additional_info": {"unit": null, "value": 2018.0}, "end": 56, "entity": "temperature", "extractor": "ner_duckling", "start": 52, "text": "2018", "value": 2018.0}, {"additional_info": {"unit": null, "value": 4000.0}, "end": 65, "entity": "temperature", "extractor": "ner_duckling", "start": 61, "text": "4000", "value": 4000.0}, {"additional_info": {"grain": "year", "others": [{"grain": "year", "value": "4000-01-01T00:00:00.000Z"}], "value": "4000-01-01T00:00:00.000Z"}, "end": 65, "entity": "time", "extractor": "ner_duckling", "start": 61, "text": "4000", "value": "4000-01-01T00:00:00.000Z"}, {"additional_info": {"grain": "day", "others": [{"grain": "day", "value": "2018-12-12T00:00:00.000Z"}], "value": "2018-12-12T00:00:00.000Z"}, "end": 56, "entity": "time", "extractor": "ner_duckling", "start": 45, "text": "12 dec 2018", "value": "2018-12-12T00:00:00.000Z"}], "intent": {"confidence": 0.8052340502832008, "name": "create_wp"}, "intent_ranking": [{"confidence": 0.8052340502832008, "name": "create_wp"}, {"confidence": 0.07587500737044468, "name": "device_failure"}, {"confidence": 0.0658988174685606, "name": "dont_know"}, {"confidence": 0.05299212487779405, "name": "greet"}], "text": "I have a new work packag from Pavan starting 12 dec 2018 for 4000 pounds"}}'
 var obj = JSON.parse(str).user_input.entities;
 
@@ -17,6 +17,11 @@ var srch = _.filter(obj, function(n){
     return  n.extractor=="ner_duckling"})
 console.log(srch)
 
+
+var test = _.filter(meta, function(n){
+    return  n.ent=="amount"})
+console.log('fg' + test)
+
 /*
 console.log(JSON.stringify(filtered))
 console.log(JSON.stringify(filtereda))
@@ -25,7 +30,10 @@ console.log(JSON.stringify(filtereda[1]))
 for(var i = 0; i < filter_ent.length; i++) {
     var start = filter_ent[i].start;
     var end = filter_ent[i].end
-
+    var enti    = filter_ent[i].entity
+    
+    
+    
     var filter_srch = _.filter(srch, function(x){
     return  x.start.toInt >=start.toInt && x.end.toInt <=end.toInt })
     
