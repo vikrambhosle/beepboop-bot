a41f91d3de173b1f861fb8ad65e1232e8fe76571
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-22 17:03:27 +0530

    Update compound.js

--- compound.js
+++ compound.js
@@ -39,12 +39,14 @@ for(var i = 0; i < filter_ent.length; i++) {
         if( metadata[0].search=="entities") { 
     var filter_srch = _.filter(srch, function(x){
     return  parseInt(x.start) >=parseInt(start) && parseInt(x.end) <=parseInt(end) && x.entity == metadata[0].type})
-     }
+    filter_ent[i]["duckling_value"]=filter_srch.value
+        }
     
          if( metadata[0].search=="additional_info.grain") { 
     var filter_srch = _.filter(srch, function(x){
     return  parseInt(x.start) >=parseInt(start) && parseInt(x.end) <=parseInt(end) && x.additional_info.grain == metadata[0].type})
-     }
+     filter_ent[i]["duckling_value"]=filter_srch.value
+    }
     
     
     ////
