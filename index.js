a6a580843e320d5a8284550fb9f4f0a62ded4ca0
vikrambhosle <vikrambhosle@hotmail.com>
2017-08-24 06:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ controller.hears(['create_wp'],'direct_message,direct_mention,mention', rasa.hea
         callback: function(response,onvo) {
            console.log('check1'+JSON.stringify(response.entities))
            console.log('check2'+JSON.stringify(response.entities))
-           console.log('dekho'+tools.composite(response.entities))
+           console.log('dekho'+JSON.stringify(tools.composite(response.entities)))
           console.log('PIntent:', response.intent, response.intent.name);
           if(response.intent.name=='dont_know' && parseFloat(response.intent.confidence)>0.40) {
           onvo.say('I need a description to proceed');
