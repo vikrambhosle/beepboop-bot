33d22c4dad3493e9a4e415529cba78fd93440be0
vikrambhosle <vikrambhosle@hotmail.com>
2017-09-10 13:10:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var tls = require('tls');
 var fs = require('fs');
 
 var options = {
-  key: fs.readFileSync('server-key.pem'),
-  cert: fs.readFileSync('server-cert.pem')
+  key: fs.readFileSync('/home/vikrambhosle402/server-key'),
+  cert: fs.readFileSync('/home/vikrambhosle402/server-crt')
 };
 
 tls.createServer(options, function (s) {
